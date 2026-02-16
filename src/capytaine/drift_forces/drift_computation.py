79d0a5566a0a2b402b11aab363848482f795b745
Schmitt Julia <julia.schmitt@mews-labs.com>
2026-02-16 14:57:35 +0100

    example

--- src/capytaine/drift_forces/drift_computation.py
+++ src/capytaine/drift_forces/drift_computation.py
@@ -46,44 +46,6 @@ def far_field(X, dataset, a):
 
     return F
 
-def plot_far_field():
-    g = _default_parameters["g"]
-    rho = _default_parameters["rho"]
-    a = 1
-    wave_direction = 0
-    radius = 1
-    theta_range = np.linspace(0, 2*np.pi, 19)
-    theta_range = np.sort(np.append(theta_range, wave_direction)) if not np.any(np.isclose(theta_range, wave_direction)) else theta_range
-
-    mesh = cpt.mesh_sphere(radius=radius, resolution=(50, 50), center=(0,0,0)).immersed_part()
-    body = cpt.FloatingBody(
-            mesh=mesh,
-            dofs=cpt.rigid_body_dofs(rotation_center=(0,0,0)),
-            name=mesh.name,
-            center_of_mass=(0,0,0),
-            ).immersed_part()
-    body.inertia_matrix = body.compute_rigid_body_inertia()
-    body.hydrostatic_stiffness = body.compute_hydrostatic_stiffness()
-
-    F_analytic=[0, 0, 0.07, 0.26, 0.49, 0.70, 0.83, 0.88, 0.84, 0.72, 0.66, 0.655, 0.652]
-    ka_analytic = np.array([0.3, 0.5, 0.83, 0.92, 1.0, 1.05, 1.09, 1.16, 1.24, 1.39, 1.59, 1.88, 2.28])
-    F_Delhommeau= [0.035, 0.8373, 0.6700, 0.6265]
-    ka_Delhommeau= [0.77, 1.29, 1.53, 2.015]
-
-    solver = cpt.BEMSolver()
-    test_matrix = xr.Dataset(coords={
-        'wavenumber': ka_analytic, 'wave_direction': wave_direction, 'theta': theta_range, 'radiating_dof': list(body.dofs.keys()), 
-    })
-    dataset = solver.fill_dataset(test_matrix, body, hydrostatics=True)
-
-    plt.plot(ka_analytic, F_analytic, '-o', label = 'analytical')
-    plt.plot(ka_Delhommeau, F_Delhommeau, '*', label = 'Delhommeau')
-    plt.grid()
-    plt.plot(ka_analytic, far_field(dataset, a)[0]/(g*rho*a**2*radius), '-x', label='far field formulation')
-    plt.legend()
-    plt.xlabel('k*r')
-    plt.ylabel('F_drift/(rho*g*a²*r)')
-    plt.show()
 
 
 ### Mesh utilities for near field formulation ###
@@ -124,7 +86,7 @@ def water_line_integral(mesh, data):
 
 
 ### Near field formulation ###
-def near_field(X, mesh, results, solver, problems, dataset, a):
+def near_field(X, mesh, body, results, solver, dataset, a):
     rho = dataset['rho']
     g = dataset['g']
     omega = dataset['omega'].data
@@ -140,34 +102,43 @@ def near_field(X, mesh, results, solver, problems, dataset, a):
     
     forces_first_order = dataset['excitation_force'][:,0,:] + compute_radiation_forces(X, dataset)[:,0,:]
 
-    grad_phi = compute_total_velocity(solver, mesh, problems, results, X, omega)
+    grad_phi = compute_total_velocity(solver, mesh, results, X, omega)
     grad_phi_square = np.sum(np.abs(grad_phi)**2, axis=2) 
     time_der_grad_phi = compute_time_der_velocity(mesh, omega, d, grad_phi)
     pressure_field = grad_phi_square/4 + time_der_grad_phi/2
-    coef1 = np.empty([len(omega)])
+    coef1 = np.empty([len(omega),3])
     for i in range(len(omega)):
-        coef1[i] = rho*mesh.surface_integral(pressure_field[i,:]*mesh.faces_normals[:,0]) #je recupere la composante x pour l'instant 
+        for dim in range(3):
+            coef1[i,dim] = rho*mesh.surface_integral(pressure_field[i,:]*mesh.faces_normals[:,dim])  
 
     edges, faces = edges_water_line(mesh)
     vertices_left = mesh.vertices[edges[:,0],:]
     vertices_right = mesh.vertices[edges[:,1],:]
     vertices_middle = ((vertices_left + vertices_right)/2) #[:,:-1]
     normal = compute_faces_normals(mesh.vertices, faces)
-    n_x = normal[:,0] / np.sqrt(1-normal[:,-1]**2)
+    # n = normal[:,0] / np.sqrt(1-normal[:,-1]**2)
 
-    eta =  compute_free_surface_elevation(solver, vertices_middle, problems, results, X, omega)
+    eta =  compute_free_surface_elevation(solver, vertices_middle, results, X, omega)
     d3 = d(vertices_middle)[:,:,-1]
-    coef2 = np.empty([len(omega)])
+    coef2 = np.zeros([len(omega),3])
     for i in range(len(omega)):
-        coef2[i] = (rho*g/4)*water_line_integral(mesh, (np.abs(eta[i,:] - d3[i,:])**2*n_x))  #je recupere la composante x pour l'instant
+        for dim in range(2):
+            coef2[i,dim] = (rho*g/4)*water_line_integral(mesh, (np.abs(eta[i,:] - d3[i,:])**2*normal[:,dim])) 
 
-    coef3 = np.empty([len(omega)])
+    coef3 = np.empty([len(omega),3])
     for i in range(len(omega)):
-        coef3[i] = 0.5*np.real(np.cross(X[i,0,3:], np.conjugate(forces_first_order[i,0:3])))[0]
+        coef3[i,:] = 0.5*np.real(np.cross(X[i,0,3:], np.conjugate(forces_first_order[i,0:3])))
 
-    return a**2*(coef1 - coef2) # + coef3)
+    coef4 = np.zeros([len(omega),3])
+    S = body.hydrostatic_stiffness
+    for i in range(len(omega)):
+        coef4[i,-1] = 1/4*(S[2,2]*(np.abs(X[i,:,3])**2 + np.abs(X[i,:,4])**2) \
+                        - S[2,3]* np.real(X[i,:,4]*np.conjugate(X[i,:,5])) \
+                        + S[2,4]*np.real(X[i,:,3]*np.conjugate(X[i,:,5])))
 
-def compute_free_surface_elevation(solver, vertices_middle, problems, results, X, omega):
+    return a**2*(coef1 - coef2 + coef3 + coef4), pressure_field
+
+def compute_free_surface_elevation(solver, vertices_middle, results, X, omega):
     len_omega = len(omega)
     size = np.shape(vertices_middle)[0]
     eta_incident = np.empty([len_omega,size], dtype=complex)
@@ -175,16 +146,11 @@ def compute_free_surface_elevation(solver, vertices_middle, problems, results, X
     eta_radiation = np.zeros([len_omega,size], dtype=complex)
 
     idx_omega = 0
-    for pb in problems:
-        if isinstance(pb, DiffractionProblem):
-            eta_incident[idx_omega,:] = airy_waves_free_surface_elevation(vertices_middle, pb)
-            idx_omega += 1
-
-    idx_omega_diff = 0 
-    for res in results: 
+    for res in results:
         if isinstance(res, DiffractionResult):
-            eta_diffraction[idx_omega_diff,:] = solver.compute_free_surface_elevation(vertices_middle, res)
-            idx_omega_diff += 1 
+            eta_incident[idx_omega,:] = airy_waves_free_surface_elevation(vertices_middle, res)
+            eta_diffraction[idx_omega,:] = solver.compute_free_surface_elevation(vertices_middle, res) 
+            idx_omega += 1
 
     for idx_omg, omg in enumerate(omega):
         for idx_res, res in enumerate(results):
@@ -203,23 +169,18 @@ def compute_time_der_velocity(mesh, omega, d, grad_phi):
     return time_der_grad_phi
 
 
-def compute_total_velocity(solver, mesh, problems, results, X, omega):
+def compute_total_velocity(solver, mesh, results, X, omega):
     len_omega = len(omega)
     grad_phi_incident = np.empty([len_omega,mesh.nb_faces,3], dtype=complex)
     grad_phi_diffraction = np.empty([len_omega,mesh.nb_faces,3], dtype=complex)
     grad_phi_radiation = np.zeros([len_omega,mesh.nb_faces,3], dtype=complex)
 
     idx_omega = 0
-    for pb in problems:
-        if isinstance(pb, DiffractionProblem):
-            grad_phi_incident[idx_omega,:,:] = airy_waves_velocity(mesh, pb)
-            idx_omega += 1
-
-    idx_omega_diff = 0 
-    for res in results: 
+    for res in results:
         if isinstance(res, DiffractionResult):
-            grad_phi_diffraction[idx_omega_diff,:,:] = solver._compute_potential_gradient(mesh, res)
-            idx_omega_diff += 1 
+            grad_phi_incident[idx_omega,:,:] = airy_waves_velocity(mesh, res)
+            grad_phi_diffraction[idx_omega,:,:] = solver._compute_potential_gradient(mesh, res)
+            idx_omega += 1
 
     for idx_omg, omg in enumerate(omega):
         for idx_res, res in enumerate(results):
@@ -242,7 +203,6 @@ def compute_radiation_forces(X, dataset):
     return F
 
 
-
 def test_curvilinear_integration():
     radius = 1
     mesh = cpt.mesh_sphere(radius=radius, resolution=(50, 50), center=(0,0,0)).immersed_part()
@@ -251,7 +211,10 @@ def test_curvilinear_integration():
     assert np.isclose(integral, 2*np.pi*radius, rtol=1e-3), "Curvilinear integration along water line failed"
 
 
-if __name__ == "__main__":
+
+
+### EXAMPLE ### 
+def hemisphere_example():
     g = _default_parameters["g"]
     rho = _default_parameters["rho"]
     a = 1
@@ -260,12 +223,15 @@ if __name__ == "__main__":
     radius = 1
     theta_range = np.linspace(0, 2*np.pi, 19)
 
-    mesh = cpt.mesh_sphere(radius=radius, resolution=(50, 50), center=(0,0,0)).immersed_part()
+    resolution = (50,50)
+    mesh = cpt.mesh_sphere(radius=radius, resolution=resolution, center=(0,0,0)).immersed_part()
+    lid_mesh = mesh.generate_lid()
     body = cpt.FloatingBody(
             mesh=mesh,
             dofs=cpt.rigid_body_dofs(rotation_center=(0,0,0)),
             name=mesh.name,
             center_of_mass=(0,0,0),
+            lid_mesh=lid_mesh,
             ).immersed_part()
     body.inertia_matrix = body.compute_rigid_body_inertia()
     body.hydrostatic_stiffness = body.compute_hydrostatic_stiffness()
@@ -278,33 +244,138 @@ if __name__ == "__main__":
     ]
     problems.extend([cpt.DiffractionProblem(body=body, wavenumber=ki, wave_direction=wave_direction) for ki in k])
 
-
     res = solver.solve_all(problems)
-    test_matrix = xr.Dataset(coords={
-        'wavenumber': k, 'wave_direction': wave_direction, 'theta': theta_range, 'radiating_dof': list(body.dofs.keys())
-    })
     dataset = assemble_dataset(res)
     kochin = kochin_data_array(res, theta_range)
     dataset.update(kochin)
 
     X = rao(dataset)
 
-    # far_field_value = far_field(X, dataset, a)[0]
-    # print("Far field value:", far_field_value)
-    # # value 634.22990511 8172.31893515
-    # near_field_value = near_field(X, mesh, res, solver, problems, dataset, a)
-    # print("Near field value:", near_field_value)
+    F, pressure_field = near_field(X, mesh, body, res, solver, dataset, a)
+    # mesh.show(backend="pyvista", color_field=pressure_field[5,:], cmap=plt.get_cmap("hot"))
 
     F_analytic=[0, 0, 0.07, 0.26, 0.49, 0.70, 0.83, 0.88, 0.84, 0.72, 0.66, 0.655, 0.652]
     F_Delhommeau= [0.035, 0.8373, 0.6700, 0.6265]
     ka_Delhommeau= [0.77, 1.29, 1.53, 2.015]
+    factor = rho*g*a**2*radius
 
-    plt.plot(k, F_analytic, '-o', label = 'analytical')
-    plt.plot(ka_Delhommeau, F_Delhommeau, '*', label = 'Delhommeau')
-    plt.grid()
-    plt.plot(k, far_field(X, dataset, a)[0]/(g*rho*a**2*radius), '-x', label='far field formulation')
-    plt.plot(k, near_field(X, mesh, res, solver, problems, dataset, a)/(g*rho*a**2*radius), '-v', label='near field formulation')
+    # plt.plot(k, F_analytic, '-o', label = 'analytical')
+    # plt.plot(ka_Delhommeau, F_Delhommeau, '*', label = 'Delhommeau')
+    # plt.grid()
+    # plt.plot(k, far_field(X, dataset, a)[0]/factor, '-x', label='far field formulation')
+    plt.plot(k, F[:,-1]/factor, '-v', label='near field formulation')
     plt.legend()
     plt.xlabel('k*r')
     plt.ylabel('F_drift/(rho*g*a²*r)')
+    plt.title(f"resolution = {resolution}, with lid")
     plt.show()
+
+
+def cylinder_example():
+    g = _default_parameters["g"]
+    rho = _default_parameters["rho"]
+    a = 1
+    wave_direction = 0
+    k = np.linspace(0.1,3,20)
+    theta_range = np.linspace(0, 2*np.pi, 19)
+    radius = 1
+
+    mesh = cpt.mesh_vertical_cylinder(length=20, radius=radius, resolution=(10,50,50)).immersed_part()
+    lid_mesh = mesh.generate_lid()
+    body = cpt.FloatingBody(
+            mesh=mesh,
+            dofs=cpt.rigid_body_dofs(rotation_center=(0,0,0)),
+            name=mesh.name,
+            center_of_mass=(0,0,0),
+            lid_mesh=lid_mesh,
+            ).immersed_part()
+    body.inertia_matrix = body.compute_rigid_body_inertia()
+    body.hydrostatic_stiffness = body.compute_hydrostatic_stiffness()
+
+    solver = cpt.BEMSolver()
+    problems = [
+        cpt.RadiationProblem(body=body, radiating_dof=dof, wavenumber=ki)
+        for dof in body.dofs
+        for ki in k
+    ]
+    problems.extend([cpt.DiffractionProblem(body=body, wavenumber=ki, wave_direction=wave_direction) for ki in k])
+
+    res = solver.solve_all(problems)
+    dataset = assemble_dataset(res)
+    kochin = kochin_data_array(res, theta_range)
+    dataset.update(kochin)
+
+    X = 0*rao(dataset)
+
+    F, pressure_field = near_field(X, mesh, body, res, solver, dataset, a)
+    # mesh.show(backend="pyvista", color_field=pressure_field[5,:], cmap=plt.get_cmap("hot"))
+    factor = rho*g*radius*a**2
+
+    plt.grid()
+    plt.plot(k, far_field(X, dataset, a)[0]/factor, '-x', label='far field formulation')
+    plt.plot(k, F[:,0]/factor, '-v', label='near field formulation')
+    plt.legend()
+    plt.xlabel('w')
+    plt.ylabel('F_drift')
+    plt.show()
+
+
+def barge_example():
+    g = _default_parameters["g"]
+    rho = _default_parameters["rho"]
+    a = 1
+    wave_direction = 0
+    water_depth = 50
+    omega = np.array([0.3, 0.5, 0.8, 1., 1.2])
+    theta_range = np.linspace(0, 2*np.pi, 19)
+
+    resolution = (100,40,20)
+    L = 150
+    B = 50
+    mesh = cpt.mesh_parallelepiped(size=(L,B,20), resolution=resolution).immersed_part()
+    # lid_mesh = mesh.generate_lid()
+    lid_mesh = cpt.mesh_rectangle(size=(L,B), resolution=resolution[:2])
+    lid_mesh.show()
+    body = cpt.FloatingBody(
+            mesh=mesh,
+            dofs=cpt.rigid_body_dofs(rotation_center=(0,0,0)),
+            name=mesh.name,
+            center_of_mass=(0,0,0),
+            lid_mesh=lid_mesh,
+            ).immersed_part()
+    body.inertia_matrix = body.compute_rigid_body_inertia()
+    body.hydrostatic_stiffness = body.compute_hydrostatic_stiffness()
+
+    solver = cpt.BEMSolver()
+    problems = [
+        cpt.RadiationProblem(body=body, radiating_dof=dof, omega=omg, water_depth=water_depth)
+        for dof in body.dofs
+        for omg in omega
+    ]
+    problems.extend([cpt.DiffractionProblem(body=body, omega=omg, wave_direction=wave_direction, water_depth=water_depth) for omg in omega])
+
+    res = solver.solve_all(problems)
+    dataset = assemble_dataset(res)
+    kochin = kochin_data_array(res, theta_range)
+    dataset.update(kochin)
+
+    X = rao(dataset)
+
+    F, _ = near_field(X, mesh, body, res, solver, dataset, a)
+
+    x = dataset['omega']*np.sqrt(L/g)
+    factor = 0.5*rho*g*a**2*B**2/L 
+
+    plt.grid()
+    plt.plot(x, far_field(X, dataset, a)[0]/factor, '-x', label='far field formulation')
+    plt.plot(x, F[:,-1]/factor, '-v', label='near field formulation')
+    plt.legend()
+    plt.xlabel('k*r')
+    plt.ylabel('F_drift/(rho*g*a²*r)')
+    plt.title(f"resolution = {resolution}, with lid")
+    plt.show()
+
+
+
+if __name__ == "__main__":
+    hemisphere_example()
